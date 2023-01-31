0b6c0aec4de3086210ab19b690393d19cf267632
ward <ward@petrus-games.eu>
2023-01-31 05:54:03 +0100

    added animation on PlayerSprite

--- Assets/Scripts/PlayerStrike.cs
+++ Assets/Scripts/PlayerStrike.cs
@@ -8,11 +8,17 @@ public class PlayerStrike : MonoBehaviour
     public Sprite NoArmSprite,defaultSprite;
     public GameObject ArmLeft,ArmRight;
 
+    private void Start()
+    {
+        ArmLeft.SetActive(false);
+        ArmRight.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1"))
         {
+            Debug.Log("jump or fire1 pressed");
             PlayerSprite.GetComponent<SpriteRenderer>().sprite = NoArmSprite;
 
             if(PlayerSprite.GetComponent<SpriteRenderer>().flipX)
@@ -26,19 +32,42 @@ public class PlayerStrike : MonoBehaviour
                 ArmLeft.GetComponent<Animator>().SetTrigger("start");
             }
         }
-        else
-        {
-            onanimationEnd();
-           //if (ArmLeft.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("idle"))
-           // {
-           //     onanimationEnd();
-           // }
 
-           //if(ArmRight.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("idle"))
-           // {
-           //     onanimationEnd();
-           // }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal < -0.01f)
+        {
+            ArmRight.SetActive(false);
+            PlayerSprite.GetComponent<SpriteRenderer>().sprite = defaultSprite;
+
         }
+
+        if (horizontal > 0.01f)
+        {
+            ArmLeft.SetActive(false);
+            PlayerSprite.GetComponent<SpriteRenderer>().sprite = defaultSprite;
+
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            //if (ArmLeft.GetComponent<Animator>().GetBool("stop") == false)
+            //{
+            //    //onanimationEnd();
+            //}
+            //if (ArmRight.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime <= 0)
+            //{
+            //    onanimationEnd();
+            //}
+
+            //if (ArmLeft.GetComponent<Animator>().GetBool("stop"))
+            //{
+            //    onanimationEnd();
+            //} //onanimationEnd();
+        }
+           
+        
 
         
     }
